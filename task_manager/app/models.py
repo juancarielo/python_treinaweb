79fc82189c4bd173afab88e99ed97ff13eb4efec
Juan Carielo <juancarielo@gmail.com>
2019-09-11 15:51:51 -0300

    Created a migration and a simple crud

--- task_manager/app/models.py
+++ task_manager/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('A', 'Alta'),
+        ('N', 'Normal'),
+        ('B', 'Baixa'),
+    ]
+    title = models.CharField(max_length=30, null=False, blank=False)
+    description = models.CharField(max_length=100, null=False, blank=False)
+    date_expiration = models.DateField(null=False, blank=False)
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, null=False, blank=False)
--- task_manager/app/urls.py
+++ task_manager/app/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 
+
 urlpatterns = [
     path('list/', list, name='list'),
+    path('create/', create, name='create'),
+    path('edit/<int:id>', edit, name='edit'),
+    path('delete/<int:id>', delete, name='delete'),
 ]
